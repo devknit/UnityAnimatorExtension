729d048eafcaf3ffb0bf060be4378d8cad3af0b8
sonobe yuichi <sonobe.yuichi@studio51.co.jp>
2026-02-25 12:30:28 +0900

    AnimationEventに対しての拡張メソッドを追加

---
+++ Editor/AnimatorExtensionEditor.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace UnityEditor
+{
+	public static class AnimatorExtensionEditor
+	{
+		sealed class FontAssetBuildProcessor : IPreprocessBuildWithReport
+		{
+			public void OnPreprocessBuild( BuildReport report)
+			{
+				Clean( true);
+			}
+			public int callbackOrder
+			{
+				get{ return 0; }
+			}
+		}
+		sealed class FontAssetSaveProcessor : AssetModificationProcessor
+		{
+			static string[] OnWillSaveAssets( string[] paths)
+			{
+				Clean( false);
+				return paths;
+			}
+		}
+		[MenuItem( "Window/Animation/Clear Animation Event")]
+		static void Clean()
+		{
+			Clean( true);
+		}
+		static void Clean( bool save)
+		{
+			string[] assetGUIDs = AssetDatabase.FindAssets( "t:AnimationClip");
+			if( (assetGUIDs?.Length ?? 0) > 0)
+			{
+				foreach( string assetGUID in assetGUIDs)
+				{
+					string assetPath = AssetDatabase.GUIDToAssetPath( assetGUID);
+					AnimationClip clip = AssetDatabase.LoadAssetAtPath<AnimationClip>( assetPath);
+					if( clip != null)
+					{
+						clip.ClearAnimationEvent();
+					}
+				}
+				if( save != false)
+				{
+					AssetDatabase.SaveAssets();
+				}
+			}
+		}
+	}
+}
---
+++ Runtime/AnimatorExtension.cs
@@ -0,0 +1,29 @@
+﻿
+namespace UnityEngine
+{
+	public static class AnimatorExtension
+	{
+		public static void ClearAnimationEvent( this AnimationClip clip)
+		{
+			if( clip != null)
+			{
+				clip.events = System.Array.Empty<AnimationEvent>();
+			}
+		}
+		public static void AddAnimationEvent( this AnimationClip clip, string methodName, float t=-1)
+		{
+			if( clip != null)
+			{
+				if( t < 0.0f)
+				{
+					t = clip.length;
+				}
+				clip.AddEvent( new AnimationEvent
+				{
+					functionName = methodName,
+					time = t
+				});
+			}
+		}
+	}
+}
